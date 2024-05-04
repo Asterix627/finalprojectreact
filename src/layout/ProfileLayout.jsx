b6efb55300e0724a6844cd98654c413732112e37
Asterix627 <alwikhafi27@gmail.com>
2024-05-04 19:51:32 +0700

    pullreq Andri

--- src/layout/ProfileLayout.jsx
+++ src/layout/ProfileLayout.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import EkskulContainer from '../components/molecules/EkskulContainer'
 import VisiMisiContainer from '../components/molecules/VisiMisiContainer'
+import DaftarGuruContainer from '../components/molecules/DaftarGuruContainer'
+import Nav from "../components/molecules/Nav"
+import Footer from "../components/molecules/Footer"
 
 const ProfileLayout = () => {
   return (
     <div>
+      <Nav/>
       <DaftarGuruContainer/>
       <VisiMisiContainer/>
       <EkskulContainer/>
+      <Footer />
     </div>
   )
 }
