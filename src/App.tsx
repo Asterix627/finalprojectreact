1d90148f47f92d74098222c1378e104dc1c573ad
ngopi santuy <ngopisantuy2225@gmail.com>
2024-05-04 15:28:44 +0700

    first edit

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import Lightbox from "yet-another-react-lightbox";
+import { slides } from "./data";
+import "yet-another-react-lightbox/styles.css";
+import {
+  Captions,
+  Download,
+  Fullscreen,
+  Thumbnails,
+  Zoom,
+} from "yet-another-react-lightbox/plugins";
+import "yet-another-react-lightbox/plugins/captions.css";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
+import Images from "./images";
+
+function App() {
+  const [index, setIndex] = useState<number>(-1);
+
+  return (
+    <>
+      <h2>GALLERY</h2>
+      <br></br>
+
+      <Images
+        data={slides}
+        onClick={(currentIndex) => setIndex(currentIndex)}
+      />
+
+      <Lightbox
+        plugins={[Captions, Download, Fullscreen, Zoom, Thumbnails]}
+        captions={{
+          showToggle: true,
+          descriptionTextAlign: "end",
+        }}
+        index={index}
+        open={index >= 0}
+        close={() => setIndex(-1)}
+        slides={slides}
+      />
+    </>
+  );
+}
+
+export default App;
---
+++ src/data.ts
@@ -0,0 +1,77 @@
+export const slides = [
+  {
+    src: "https://ik.imagekit.io/galeri/sekolah/berkuda.jpg?updatedAt=1714750757565",
+    title: "Ekstrakulikuler",
+    description: "Berkuda\n\nby Phootograp SMP ISLAM ASQOLAN",
+  },
+  {
+    src: "https://ik.imagekit.io/galeri/sekolah/futsal.jpg?updatedAt=1714750757076",
+    title: "Ekstrakulikuler",
+    description: "Futsal\n\nby Phootograp SMP ISLAM ASQOLAN",
+  },
+  {
+    src: "https://ik.imagekit.io/galeri/sekolah/silat.jpg?updatedAt=1714750748617",
+    title: "Ekstrakulikuler",
+    description: "Silat\n\nby Phootograp SMP ISLAM ASQOLAN",
+  },
+  {
+    src: "https://ik.imagekit.io/galeri/sekolah/habsy.jpeg?updatedAt=1714750729591",
+    title: "Ekstrakulikuler",
+    description: "Hasby\n\nby Phootograp SMP ISLAM ASQOLAN",
+  },
+  {
+    src: "https://ik.imagekit.io/galeri/sekolah/sekolah4.jpg?updatedAt=1714751213895",
+    title: "Diskusi",
+    description: "by Phootograp SMP ISLAM ASQOLAN",
+  },
+  {
+    src: "https://ik.imagekit.io/galeri/sekolah/sekolah3.jpg?updatedAt=1714751213790",
+    title: "Diskusi",
+    description: "by Phootograp SMP ISLAM ASQOLAN",
+  },
+  {
+    src: "https://ik.imagekit.io/galeri/sekolah/sekolah2.jpg?updatedAt=1714751213731",
+    title: "Baca Qur'an",
+    description: "by Phootograp SMP ISLAM ASQOLAN",
+  },
+  {
+    src: "https://ik.imagekit.io/galeri/sekolah/sekolah1.jpg?updatedAt=1714750739784",
+    title: "SMP ASQOLAN",
+    description: "by Phootograp SMP ISLAM ASQOLAN",
+  },
+  {
+    src: "https://ik.imagekit.io/galeri/sekolah/prestasi1.jpg?updatedAt=1714750752801",
+    title: "Prestasi",
+    description: "by Phootograp SMP ISLAM ASQOLAN",
+  },
+  {
+    src: "https://ik.imagekit.io/galeri/sekolah/prestasi4.jpg?updatedAt=1714750729582",
+    title: "Prestasi",
+    description: "by Phootograp SMP ISLAM ASQOLAN",
+  },
+  {
+    src: "https://ik.imagekit.io/galeri/sekolah/prestasi4.jpg?updatedAt=1714750729582",
+    title: "Photo Siswa",
+    description: "by Phootograp SMP ISLAM ASQOLAN",
+  },
+  {
+    src: "https://ik.imagekit.io/galeri/sekolah/prestasi3.jpg?updatedAt=1714750729033",
+    title: "Photo Siswi",
+    description: "by Phootograp SMP ISLAM ASQOLAN",
+  },
+  {
+    src: "https://ik.imagekit.io/galeri/sekolah/prestasi2.jpg?updatedAt=1714750727642",
+    title: "Photo Siswi",
+    description: "by Phootograp SMP ISLAM ASQOLAN",
+  },
+  {
+    src: "https://ik.imagekit.io/galeri/sekolah/panah.jpg?updatedAt=1714750726931",
+    title: "Extrkulikuler",
+    description: "Panah\n\nby Phootograp SMP ISLAM ASQOLAN",
+  },
+  {
+    src: "https://ik.imagekit.io/galeri/sekolah/logosekolah.png?updatedAt=1714750748862",
+    title: "Logo Sekolah",
+    description: "by Design SMP ISLAM ASQOLAN",
+  },
+];
